21b710139af89a7b5509336dcf3ef09cf759c39e
naohito-T <naohito.tanaka0523@gmail.com>
2022-03-19 02:12:54 +0900

    :white_check_mark: jestを設置

---
+++ portfolio-ui/jest.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^~/(.*)$': '<rootDir>/src/$1',
+    '^vue$': 'vue/dist/vue.common.js'
+  },
+  moduleFileExtensions: ['ts', 'js', 'vue', 'node'],
+  testPathIgnorePatterns: ['<rootDir>/cypress'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.js$': 'babel-jest',
+    '.*\\.(vue)$': 'vue-jest'
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '<rootDir>/components/**/*.vue',
+    '<rootDir>/pages/**/*.vue'
+  ],
+  testEnvironment: 'jsdom'
+};
+
+export default config;
