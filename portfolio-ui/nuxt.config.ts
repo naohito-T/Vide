ce3210340270eb773d4714c4ab408b839093e798
naohito-T <naohito.tanaka0523@gmail.com>
2022-03-19 13:26:08 +0900

    :test_tube: yarn upgradeで更新をかけるためpush

--- portfolio-ui/nuxt.config.ts
+++ portfolio-ui/nuxt.config.ts
@@ -1,30 +1,30 @@
-import { NuxtConfig } from "@nuxt/types";
+import { NuxtConfig } from '@nuxt/types';
 
 const nuxtConfig: NuxtConfig = {
   // Global page headers: https://go.nuxtjs.dev/config-head
-  srcDir: "src/",
+  srcDir: 'src/',
   head: {
-    title: "portfolio-ui",
+    title: 'portfolio-ui',
     htmlAttrs: {
-      lang: "en",
+      lang: 'en'
     },
     meta: [
-      { charset: "utf-8" },
-      { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
-      { name: "format-detection", content: "telephone=no" },
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
   env: {
-    NODE_ENV: process.env.NODE_ENV ?? "local",
+    NODE_ENV: process.env.NODE_ENV ?? 'local'
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{ src: "@/plugins/stores" }],
+  plugins: [{ src: '@/plugins/stores' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -32,30 +32,30 @@ const nuxtConfig: NuxtConfig = {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    "@nuxt/typescript-build",
-    "@nuxtjs/composition-api/module",
+    '@nuxt/typescript-build',
+    '@nuxtjs/composition-api/module'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    "@nuxtjs/axios",
+    '@nuxtjs/axios'
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: "/",
+    baseURL: '/'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     extend(config, { isClient }) {
       if (isClient) {
-        config.devtool = "source-map";
+        config.devtool = 'source-map';
       }
-    },
-  },
+    }
+  }
 };
 
 export default nuxtConfig;
