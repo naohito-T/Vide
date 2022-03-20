ac1f2a2d07bdc4d929767ea621d3b48acc7aa875
naohito-T <naohito.tanaka0523@gmail.com>
2022-03-20 12:20:56 +0900

    :recycle: sass setupにbranch名を変更した

---
+++ portfolio-ui/src/pages/works/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>works</div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  useAsync,
+  useContext,
+  useRoute,
+  computed
+} from '@nuxtjs/composition-api';
+
+export default defineComponent({
+  setup() {
+    return {};
+  }
+});
+</script>
