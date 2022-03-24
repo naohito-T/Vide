74df2c94b0ef7ec035b0b66a309b48982333c533
naohito-T <naohito.tanaka0523@gmail.com>
2022-03-25 01:19:44 +0900

    :construction: works pageほぼ完成

--- portfolio-ui/src/utils/tests/index.test.ts
+++ portfolio-ui/src/utils/tests/index.test.ts
@@ -4,4 +4,9 @@ describe('Utils Test.', () => {
   test('arrayFactorys method test', () => {
     expect(arrayFactorys('s', 5).length).toBe(5);
   });
+
+  // まだ途中
+  // test('randomString method test', () => {
+  //   expect(arrayFactorys('s', 5).length).toBe(5);
+  // })
 });
